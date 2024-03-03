460b21a79808676bb28a363d3faa19c90ca55049
ChanJungTW <zhanrong@msn.com>
2024-03-03 20:53:12 +0800

    Add Product Function-deleteProduct.

--- src/main/java/com/johnhsu/springbootbookstore/controller/ProductController.java
+++ src/main/java/com/johnhsu/springbootbookstore/controller/ProductController.java
@@ -42,5 +42,10 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+        productService.deleteProductById(productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }

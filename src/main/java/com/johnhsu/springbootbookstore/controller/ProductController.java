b4bfbd2d5bcaf6f81a6415c6ab710a6f74c335ca
ChanJungTW <zhanrong@msn.com>
2024-03-05 21:09:24 +0800

    Add Product Function-improve getProducts method-(limit+offset keywords).

--- src/main/java/com/johnhsu/springbootbookstore/controller/ProductController.java
+++ src/main/java/com/johnhsu/springbootbookstore/controller/ProductController.java
@@ -6,15 +6,18 @@ import com.johnhsu.springbootbookstore.model.Product;
 import com.johnhsu.springbootbookstore.service.ProductService;
 import constant.ProductCategory;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -24,13 +27,19 @@ public class ProductController {
     public ResponseEntity<List<Product>> getProducts(@RequestParam(required=false) ProductCategory category,
                                                      @RequestParam(required = false) String search,
                                                      @RequestParam(defaultValue="created_date") String orderBy,
-                                                     @RequestParam(defaultValue="desc") String sort)
+                                                     @RequestParam(defaultValue="desc") String sort,
+                                                     @RequestParam(defaultValue="2") @Max(1000) @Min(0) Integer limit,
+                                                     @RequestParam(defaultValue ="0 ") @Min(0) Integer offset
+
+                                                     )
     {
         ProductQueryParams productQueryParams=new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList=productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);

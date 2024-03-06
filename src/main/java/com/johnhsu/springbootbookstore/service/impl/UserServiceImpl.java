965811e163fe5f64eea8f46e46848c37c3d58e82
ChanJungTW <zhanrong@msn.com>
2024-03-06 19:11:51 +0800

    User Function improved,(Check Email,and Password is hided).

--- src/main/java/com/johnhsu/springbootbookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/johnhsu/springbootbookstore/service/impl/UserServiceImpl.java
@@ -4,11 +4,16 @@ import com.johnhsu.springbootbookstore.dao.UserDao;
 import com.johnhsu.springbootbookstore.dto.UserRegisterRequest;
 import com.johnhsu.springbootbookstore.model.User;
 import com.johnhsu.springbootbookstore.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+    private final static Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +24,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user=userDao.getUserByEmail(userRegisterRequest.getEmail());
+        if(user!=null){
+            log.warn("The email address {} isn't available",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        }
         return userDao.createUser(userRegisterRequest);
     }
 }
